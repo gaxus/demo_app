b97f22d9863dedf37ebbf5b2aa374b70f48d9b35
root <root@gaxus-computer.gaxrez>
2012-09-21 12:23:46 +0200

    Import initial.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '96da00fbe6b9467c9c8d8bcdf49a0a03423cae8dc57528d836288ddb98c6c8a1739bb79062dbcbe2a71cb4882ccdb475a832b4e63043cda6386a7cb72c60857e'
